d3f07a49111677994527be110ceccc746de2851a
roger yang <roger.yang@nike.com>
2019-11-20 16:39:11 +0800

    add discovery

--- src/main/java/com/longge/nacos/NacosClientApplication.java
+++ src/main/java/com/longge/nacos/NacosClientApplication.java
@@ -2,12 +2,14 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author roger yang
  * @date 11/20/2019
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NacosClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosClientApplication.class, args);
